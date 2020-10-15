7ed255dc6849f3cd94c9f19b6f56a5148d3e1de5
ajisuryawan <kazumovie132@gmail.com>
2020-10-15 14:42:38 +0700

    update terbaru

--- app/src/main/java/com/example/belajarandroidselasa/MenuUtama.java
+++ app/src/main/java/com/example/belajarandroidselasa/MenuUtama.java
@@ -3,12 +3,28 @@ package com.example.belajarandroidselasa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MenuUtama extends AppCompatActivity {
+
+    ImageButton ivsgd;
+    ImageButton ivusd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_utama);
+        ivsgd = (ImageButton) findViewById(R.id.ivsgd);
+        ivusd = (ImageButton) findViewById(R.id.ivusd);
+
+        ivsgd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/belajarandroidselasa/SgdToIdr.java
@@ -0,0 +1,14 @@
+package com.example.belajarandroidselasa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SgdToIdr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sgd_to_idr);
+    }
+}
